f8e350bd045e9b2796bdfe722c84b52e30aa79da
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-06-19 17:57:09 +0200

    feat: added index page with project table

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>HI</h1>
+    <div class="container py-5">
+        <h1 class="text-center">Project Table</h1>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Slug</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($projectsArray as $project)
+                    <tr>
+                        <th scope="row"> {{ $project->title }} </th>
+                        <td> {{ $project->description }} </td>
+                        <td> {{ $project->slug }} </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
