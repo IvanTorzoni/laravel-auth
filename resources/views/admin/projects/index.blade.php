324ccc2ac9dcd5daf57fe84bbada73498129b022
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-06-24 14:13:21 +0200

    feat: added small elements of style to buttons and icons in dashboard navbar

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -6,14 +6,14 @@
 
         <div>
             <form action="">
-                <label for="perPage">elementi in pagina</label>
+                <label for="perPage">Select the number of elements in page</label>
                 <select name="perPage" id="perPage">
-                  <option value="5" @selected($projectsArray->perPage() == 5)>5</option>
-                  <option value="10" @selected($projectsArray->perPage() ==10)>10</option>
-                  <option value="15" @selected($projectsArray->perPage() == 15)>15</option>
+                    <option value="5" @selected($projectsArray->perPage() == 5)>5</option>
+                    <option value="10" @selected($projectsArray->perPage() == 10)>10</option>
+                    <option value="15" @selected($projectsArray->perPage() == 15)>15</option>
                 </select>
 
-                <button type="submit">Apply</button>
+                <button type="submit" class="btn btn-primary">Apply</button>
             </form>
         </div>
 
@@ -38,17 +38,20 @@
                         <th scope="row"> {{ $project->title }} </th>
                         <td> {{ $project->description }} </td>
                         <td> {{ $project->slug }} </td>
-                        <td> <a class='btn btn-info'
-                                href="{{ route('admin.projects.show', ['project' => $project->slug]) }}">Details</a>
-                             <a class='btn btn-warning'
-                                href="{{ route('admin.projects.edit', ['project' => $project->slug]) }}">Modify</a>
+                        <td>
+                            <div class="d-flex align-item-center">
+                                <a class='btn btn-info'
+                                    href="{{ route('admin.projects.show', ['project' => $project->slug]) }}"><i class="fa-solid fa-circle-info" title="Details"></i></a>
+                                <a class='btn btn-warning'
+                                    href="{{ route('admin.projects.edit', ['project' => $project->slug]) }}"><i class="fa-solid fa-pen" title="Modify"></i></a>
 
-                            <form action="{{ route('admin.projects.destroy', ['project' => $project->slug]) }}"
-                                method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button class="btn btn-danger" type="submit">Delete</button>
-                            </form>
+                                <form action="{{ route('admin.projects.destroy', ['project' => $project->slug]) }}"
+                                    method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger" type="submit"><i class="fa-solid fa-trash" title="Delete"></i></button>
+                                </form>
+                            </div>
                         </td>
                     </tr>
                 @endforeach
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -60,13 +60,13 @@
               <li class="nav-item">
                 <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.dashboard' ? 'bg-secondary' : '' }}"
                   href="{{ route('admin.dashboard') }}">
-                  <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Dashboard
+                  <i class="fa-solid fa-table-columns"></i> Dashboard
                 </a>
               </li>
               <li class="nav-item">
                 <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.projects.index' ? 'bg-secondary' : '' }}"
                   href="{{ route('admin.projects.index') }}">
-                  <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Projects
+                  <i class="fa-solid fa-diagram-project"></i> Projects
                 </a>
               </li>
               <li class="nav-item">
