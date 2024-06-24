31c2aa1e5993c8271780182b6113ec1787d86703
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-06-24 15:09:11 +0200

    feat: added button for redirection to index page for user experience

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -11,13 +11,20 @@
             @method('PUT')
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $project->title) }}">
+                <input type="text" class="form-control" id="title" name="title"
+                    value="{{ old('title', $project->title) }}">
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">Description</label>
                 <textarea class="form-control" id="description" name="description" rows="3">{{ old('description', $project->description) }}</textarea>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+
+            <div class="d-flex justify-content-between">
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <a href="{{ route('admin.projects.index') }}" class="btn btn-outline-primary" title="Indietro"><i
+                        class="fa-solid fa-square-caret-left"></i></a>
+            </div>
+
         </form>
     </div>
 @endsection
